7c0e822483c0d4be538198ff0f8e01b050e6c7b2
Jose Francisco Peña Estrada <jose.francisco.pees@gmail.com>
2017-06-14 15:03:08 -0500

    added request to create and delete a note from the server

--- src/services/notesService.js
+++ src/services/notesService.js
@@ -1,10 +1,15 @@
-keepApp.service('notesService', function () {
+keepApp.service('notesService', function ($http) {
+    var vm = this;
     this.data = { searchTerm: '' };
-    this.notes = [{title: 'Titulo1', content: 'Contenido1', color:'#CCC', edit: false}];
+    this.notes = [];
 
     this.addNote = function(title, content, color) {
         var note = { title: title, content: content, color: color, edit: false};
-        this.notes.push(note);
+        $http.post('http://0.0.0.0:3000/api/notes/', note)
+            .then(function(resp){
+                note.id = resp.data.id;
+                vm.notes.push(note);
+            });
     };
     this.editNote = function(index, title, content, color) {
         this.notes[index].title = title;
@@ -12,6 +17,12 @@ keepApp.service('notesService', function () {
         this.notes[index].color = color;
     }
     this.removeNote = function(index) {
+        var id = this.notes[index].id;
+        $http.delete('http://0.0.0.0:3000/api/notes/' + id);
         this.notes.splice(index, 1);
     };
+
+    this.getNotes = function() {
+        return $http.get('http://0.0.0.0:3000/api/notes');
+    }
 });
