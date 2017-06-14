b65b8eed4be9c65dd9f7911dca853c59842612c2
Jose Francisco Peña Estrada <jose.francisco.pees@gmail.com>
2017-06-14 13:21:10 -0500

    added service for manage the notes, adding, editing and deleting

--- src/services/notesService.js
+++ src/services/notesService.js
@@ -1,7 +1,17 @@
 keepApp.service('notesService', function () {
-    this.notes = [];
-    this.addNote = function(title, content) {
-        var note = { title: title, content: content};
+    this.data = { searchTerm: '' };
+    this.notes = [{title: 'Titulo1', content: 'Contenido1', color:'#CCC', edit: false}];
+
+    this.addNote = function(title, content, color) {
+        var note = { title: title, content: content, color: color, edit: false};
         this.notes.push(note);
+    };
+    this.editNote = function(index, title, content, color) {
+        this.notes[index].title = title;
+        this.notes[index].content = content;
+        this.notes[index].color = color;
     }
+    this.removeNote = function(index) {
+        this.notes.splice(index, 1);
+    };
 });
