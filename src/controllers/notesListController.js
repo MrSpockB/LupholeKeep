7cb7dca246ec561948c29b114b13961acc2b631f
Jose Francisco Peña Estrada <jose.francisco.pees@gmail.com>
2017-06-14 13:22:28 -0500

    call the service functions in the listNotes ctrl

--- src/controllers/notesListController.js
+++ src/controllers/notesListController.js
@@ -1,3 +1,12 @@
 keepApp.controller('notesListCtrl', ['$scope', 'notesService', function($scope, notesService) {
     $scope.notes = notesService.notes;
+    $scope.data = notesService.data;
+
+    $scope.removeNote = function(index) {
+        notesService.removeNote(index);
+    };
+    $scope.changeEditMode = function(index) {
+        var editValue = $scope.notes[index].edit;
+        $scope.notes[index].edit = !editValue;
+    }
 }]);
